6953062bae6605663b7cf9cad617b501d6537100
Jesse Naiman <jessenaiman@gmail.com>
2025-09-25 15:43:12 -0400

    fix: codacy issues - remove invalid SQL statements, fix jest XSS

--- jest.config.mjs
+++ jest.config.mjs
@@ -27,7 +27,7 @@ export default {
       statements: 80,
     },
   },
-  setupFilesAfterEnv: [`${'<rootDir>'}/tests/setup.ts`],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
